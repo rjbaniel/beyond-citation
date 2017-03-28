6ec80319862a2c753aa0559b72bb2ad2126c2ee1
Daniel Jones <drjones18@gmail.com>
2017-03-28 10:02:33 -0400

    Add HTML-only widget tab.

--- includes/widget_shortcode.php
+++ includes/widget_shortcode.php
@@ -18,15 +18,40 @@ function bc_display_widget_generator( $atts ) {
 
 	$database_array = get_posts( $args );
 	if ( $database_array ) :
-		$database_name = $database_array[0]->post_title;
+		$database_object = $database_array[0];
+		$database_id = $database_object->ID;
+		$database_name = $database_object->post_title;
 
 		?>
 		<button id="bc-widget-gen-button" class="bc-widget-gen__button">Embed</button>
 		<div id="bc-widget-gen-container" class="bc-widget-gen__container">
-			<p class="bc-widget-gen__description">Copy the HTML below and paste it into your website to add a widget with information about <?php echo esc_html( $database_name ) ?>. Click anywhere inside the text area to to select it for copying.</p>
-			<pre class="bc-widget-gen__html" id="bc-widget-gen-pre">
+			<div class="bc-widget-gen__embed-tabs">
+				<a class="embed-tab embed-tab--selected" data-section="embedJS">
+					Dynamic
+				</a>
+				<a class="embed-tab" data-section="embedHTML">
+					HTML-only
+				</a>
+			</div>
+
+			<div id="embedJS" class="bc-widget-gen__embed-section bc-widget-gen__embed-section--js bc_visible">
+				<p class="bc-widget-gen__description">Copy the HTML below and paste it into your website to add a widget with information about <?php echo esc_html( $database_name ) ?>. Click anywhere inside the text area to to select it for copying.</p>
+				<pre class="bc-widget-gen__code">
 &lt;div class="bc-widget" data-database="<?php echo esc_attr( $database ); ?>"&gt;&lt;/div&gt;
 &lt;script type="text/javascript" src="<?php echo esc_url( $widget_script_url ); ?>"&gt;&lt;/script&gt;</pre>
+			</div>
+			<div id="embedHTML" class="bc-widget-gen__embed-section bc-widget-gen__embed-section--html">
+				<p>Use this information if you are not able to load external Javascript files on your website. Please note that this code will <em>not</em> be
+					automatically updated when we make updates here at Beyond Citation. You will need to re-generate the embed code and replace the older version.</p>
+				<pre class="bc-widget-gen__code"><?php
+					$date = get_post_meta( $database_id, 'date_range', true );
+					$publisher = get_post_meta( $database_id, 'publisher_name', true );
+					$overview = get_post_meta( $database_id, 'bc_overview', true );
+					?>&lt;h3&gt;<?php echo esc_html( $database_name ); ?>&lt;/h3&gt;
+&lt;strong&gt;Date Range: &lt;/strong&gt;<?php echo esc_html( $date ); ?>&lt;br&gt;
+&lt;strong&gt;Publisher: &lt;/strong&gt;<?php echo esc_html( $publisher ); ?>&lt;br&gt;
+&lt;p&gt;<?php echo esc_html( $overview ); ?>&lt;/p&gt;</pre>
+			</div>
 		</div>
 		<?php
 	else :
--- widget-gen.js
+++ widget-gen.js
@@ -1,9 +1,18 @@
 var widget_gen_button = document.getElementById('bc-widget-gen-button');
 var widget_gen_container = document.getElementById('bc-widget-gen-container');
 var container_visible = false;
-var widget_gen_pre = document.getElementById('bc-widget-gen-pre');
+var widget_gen_tabs = document.getElementsByClassName('embed-tab');
+var widget_gen_sections = document.getElementsByClassName('bc-widget-gen__embed-section');
+var widget_gen_pres = document.getElementsByClassName('bc-widget-gen__code');
 
 widget_gen_button.addEventListener('click', toggleWidgetGenContainer);
+for (var i = 0; i < widget_gen_pres.length; i++) {
+	widget_gen_pres[i].addEventListener('click', selectAllHTML);
+}
+for (var i = 0; i < widget_gen_tabs.length; i++) {
+	widget_gen_tabs[i].addEventListener('click', makeSectionVisible);
+}
+
 function toggleWidgetGenContainer(event) {
 	event.preventDefault();
 	widget_gen_container.classList.toggle('bc_visible');
@@ -17,7 +26,6 @@ function toggleWidgetGenContainer(event) {
 
 }
 
-widget_gen_pre.addEventListener('click', selectAllHTML);
 function selectAllHTML(event) {
 	// taken from http://www.satya-weblog.com/2013/11/javascript-select-all-content-html-element.html
 	var range, selection;
@@ -32,5 +40,30 @@ function selectAllHTML(event) {
 		range.selectNodeContents(this);
 		selection.removeAllRanges();
 		selection.addRange(range);
+	}
+}
+
+function makeSectionVisible(event) {
+	event.preventDefault();
+
+	var target_tab = event.target;
+	target_tab.classList.add('embed-tab--selected');
+	// Un-highlight other tabs
+	for (var i = 0; i < widget_gen_tabs.length; i++) {
+		if (widget_gen_tabs[i].dataset.section != target_tab.dataset.section) {
+			widget_gen_tabs[i].classList.remove('embed-tab--selected');
+		}
+	}
+
+	var target_section_id = target_tab.dataset.section;
+	var target_section = document.getElementById(target_section_id);
+	if (target_section) {
+		target_section.classList.add('bc_visible');
+		// Hide other sections
+		for (i = 0; i < widget_gen_sections.length; i++) {
+			if (widget_gen_sections[i].id != target_section_id) {
+				widget_gen_sections[i].classList.remove('bc_visible');
+			}
+		}
 	}
 }
